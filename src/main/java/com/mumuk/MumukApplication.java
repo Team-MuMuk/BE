5a3ef49d111adda4e89e6df7e38f2c2e1084890f
parkmineum <a01087164428@gmail.com>
2025-06-26 22:27:11 +0900

    Initial Settings

---
+++ src/main/java/com/mumuk/MumukApplication.java
@@ -0,0 +1,13 @@
+package com.mumuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumukApplication.class, args);
+	}
+
+}
