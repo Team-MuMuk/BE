b052665a66838c69fdf79e6163d697181270ed4c
parkmineum <a01087164428@gmail.com>
2025-06-26 22:17:15 +0900

    Initial Settings

---
+++ mumuk/src/test/java/com/mumuk/MumukApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
