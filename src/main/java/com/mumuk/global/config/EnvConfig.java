f21582c6e0b512ea773e895f70f6786ec47670e8
parkmineum <a01087164428@gmail.com>
2025-07-01 02:13:57 +0900

    [SETTING] #4 : 운영 환경 DB 세팅

--- src/main/java/com/mumuk/global/config/EnvConfig.java
+++ src/main/java/com/mumuk/global/config/EnvConfig.java
@@ -14,10 +14,25 @@ public class EnvConfig {
     public DataSource dataSource() {
         Dotenv dotenv = Dotenv.load();      // .env 파일 자동 로드
 
+        String appEnv = dotenv.get("APP_ENV", "dev"); // 기본값은 dev
+
         HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(dotenv.get("LOCAL_DB_URL"));
-        dataSource.setUsername(dotenv.get("LOCAL_DB_USERNAME"));
-        dataSource.setPassword(dotenv.get("LOCAL_DB_PASSWORD"));
+
+        if ("prod".equalsIgnoreCase(appEnv)) {
+            String endpoint = dotenv.get("PROD_DB_ENDPOINT");
+            String dbName = dotenv.get("PROD_DB_NAME");
+            String jdbcUrl = "jdbc:postgresql://" + endpoint + ":5432/" + dbName;
+
+            dataSource.setJdbcUrl(jdbcUrl);
+            dataSource.setUsername(dotenv.get("PROD_DB_USERNAME"));
+            dataSource.setPassword(dotenv.get("PROD_DB_PASSWORD"));
+        } else {
+            // dev 환경
+            dataSource.setJdbcUrl(dotenv.get("LOCAL_DB_URL"));
+            dataSource.setUsername(dotenv.get("LOCAL_DB_USERNAME"));
+            dataSource.setPassword(dotenv.get("LOCAL_DB_PASSWORD"));
+        }
+
         return dataSource;
     }
 }
