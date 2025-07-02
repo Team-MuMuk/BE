87bb7b6e917a12f2a816bc10d50fe7f2720d7e74
parkmineum <a01087164428@gmail.com>
2025-07-03 02:41:44 +0900

    [SETTING] : EnvLoader 환경 구분 제외

--- src/main/java/com/mumuk/global/config/EnvConfig.java
+++ src/main/java/com/mumuk/global/config/EnvConfig.java
@@ -20,24 +20,28 @@ public class EnvConfig {
                 .ignoreIfMissing()
                 .load();
 
-        String appEnv = dotenv.get("APP_ENV", "dev"); // 기본값은 dev
+//        String appEnv = dotenv.get("APP_ENV", "dev");
 
         HikariDataSource dataSource = new HikariDataSource();
 
-        if ("prod".equalsIgnoreCase(appEnv)) {
-            String endpoint = dotenv.get("PROD_DB_ENDPOINT");
-            String dbName = dotenv.get("PROD_DB_NAME");
-            String jdbcUrl = "jdbc:postgresql://" + endpoint + ":5432/" + dbName;
+        dataSource.setJdbcUrl(dotenv.get("LOCAL_DB_URL"));
+        dataSource.setUsername(dotenv.get("LOCAL_DB_USERNAME"));
+        dataSource.setPassword(dotenv.get("LOCAL_DB_PASSWORD"));
 
-            dataSource.setJdbcUrl(jdbcUrl);
-            dataSource.setUsername(dotenv.get("PROD_DB_USERNAME"));
-            dataSource.setPassword(dotenv.get("PROD_DB_PASSWORD"));
-        } else {
+//        if ("prod".equalsIgnoreCase(appEnv)) {
+//            String endpoint = dotenv.get("PROD_DB_ENDPOINT");
+//            String dbName = dotenv.get("PROD_DB_NAME");
+//            String jdbcUrl = "jdbc:postgresql://" + endpoint + ":5432/" + dbName;
+//
+//            dataSource.setJdbcUrl(jdbcUrl);
+//            dataSource.setUsername(dotenv.get("PROD_DB_USERNAME"));
+//            dataSource.setPassword(dotenv.get("PROD_DB_PASSWORD"));
+//        } else {
             // dev 환경
-            dataSource.setJdbcUrl(dotenv.get("LOCAL_DB_URL"));
-            dataSource.setUsername(dotenv.get("LOCAL_DB_USERNAME"));
-            dataSource.setPassword(dotenv.get("LOCAL_DB_PASSWORD"));
-        }
+//            dataSource.setJdbcUrl(dotenv.get("LOCAL_DB_URL"));
+//            dataSource.setUsername(dotenv.get("LOCAL_DB_USERNAME"));
+//            dataSource.setPassword(dotenv.get("LOCAL_DB_PASSWORD"));
+//        }
 
         return dataSource;
     }
