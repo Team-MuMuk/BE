d249d8bb29adc577ceca97d218954b52bfbe7926
parkmineum <a01087164428@gmail.com>
2025-07-07 02:56:25 +0900

    [SETTING] #7 : Swagger JWT 설정 추가

--- src/main/java/com/mumuk/global/config/SwaggerConfig.java
+++ src/main/java/com/mumuk/global/config/SwaggerConfig.java
@@ -1,24 +1,51 @@
 package com.mumuk.global.config;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        String accessTokenSchemeName = "accessToken";
+        String refreshTokenSchemeName = "refreshToken";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(accessTokenSchemeName);
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(accessTokenSchemeName, new SecurityScheme()
+                        .name("accessToken")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"))
+                .addSecuritySchemes(refreshTokenSchemeName, new SecurityScheme()
+                        .name("refreshToken")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .info(apiInfo());
+                .components(components)
+                .info(apiInfo())
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement);
     }
 
     private Info apiInfo() {
         return new Info()
                 .title("✨오늘 뭐 해먹지💡? Swagger")
-                .description("오늘 뭐 해먹지 팀의 배포 Swagger 입니다.")
+                .description("오늘 뭐 해먹지 팀의 Swagger 입니다.")
                 .version("1.0");
     }
 }
