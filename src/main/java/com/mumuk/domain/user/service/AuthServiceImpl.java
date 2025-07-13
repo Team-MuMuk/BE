f2cf8392fc6fe2cbc2d5f6edc4894aab3ad04d69
parkmineum <a01087164428@gmail.com>
2025-07-14 00:55:33 +0900

    [FEAT] #26 : 유저 탈퇴 로직 적용

--- src/main/java/com/mumuk/domain/user/service/AuthServiceImpl.java
+++ src/main/java/com/mumuk/domain/user/service/AuthServiceImpl.java
@@ -98,12 +98,9 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     @Transactional
-    public void withdraw(String accessToken, LoginType loginType) {
-        if (accessToken == null || !accessToken.startsWith("Bearer ")) {
-            throw new AuthException(ErrorCode.JWT_INVALID_TOKEN);
-        }
-        User user = getUserFromToken(accessToken, loginType);
-
+    public void withdraw(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new AuthException(ErrorCode.USER_NOT_FOUND));
         userRepository.delete(user);
     }
 
@@ -182,7 +179,7 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     @Transactional
-    public void reissueUserPassword(AuthRequest.RecoverPassWordReq request, String accessToken) {
+    public void reissueUserPassword(AuthRequest.RecoverPassWordReq request, Long userId) {
         String newPassword = request.getPassWord();
         String confirmPassword = request.getConfirmPassWord();
 
@@ -195,8 +192,7 @@ public class AuthServiceImpl implements AuthService {
             throw new AuthException(ErrorCode.INVALID_PASSWORD_FORMAT);
         }
 
-        String phoneNumber = jwtTokenProvider.getPhoneNumberFromToken(accessToken);
-        User user = userRepository.findByPhoneNumber(phoneNumber)
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new AuthException(ErrorCode.USER_NOT_FOUND));
 
 
