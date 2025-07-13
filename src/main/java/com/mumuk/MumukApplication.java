f6e32001a63f27a096540342b151e375a37bd74e
parkmineum <a01087164428@gmail.com>
2025-07-13 16:38:37 +0900

    [CHORE] : Redis 컨테이너 추가

--- src/main/java/com/mumuk/MumukApplication.java
+++ src/main/java/com/mumuk/MumukApplication.java
@@ -1,23 +1,13 @@
 package com.mumuk;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MumukApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MumukApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner logRawEnv() {
-		return args -> {
-			System.out.println("🔍 POSTGRESQL_URL (env): " + System.getenv("POSTGRESQL_URL"));
-			System.out.println("🔍 SPRING_PROFILES_ACTIVE (env): " + System.getenv("SPRING_PROFILES_ACTIVE"));
-		};
 	}
 
 }
