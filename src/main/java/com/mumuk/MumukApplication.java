694e88a3a8effcb6d0db6f0214b181232c6dda5d
parkmineum <a01087164428@gmail.com>
2025-07-13 15:00:48 +0900

    [CHORE] : application log 추가

--- src/main/java/com/mumuk/MumukApplication.java
+++ src/main/java/com/mumuk/MumukApplication.java
@@ -1,13 +1,21 @@
 package com.mumuk;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MumukApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MumukApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner checkDbUrl(@Value("${spring.datasource.url}") String url) {
+		return args -> System.out.println("✅ 실제 DB URL: " + url);
 	}
 
 }
